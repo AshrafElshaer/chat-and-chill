32fa1706ad12770b2b8e6043782912db902f6bcc
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2023-05-06 02:16:41 -0500

    tailwind screens & colors config & css styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,31 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xsm: "375px",
+      sm: "540px",
+      md: "768px",
+      lg: "1070px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        greenBg: "#61C554",
+        lightBg :"#131313",
+        darkBg: "#000000",
+        warning : "#CC3169",
+        primary: "#FFFFFF",
+        lightGray: "#B8B8B8",
+        darkGrey: "#969696",
+        success: "#85E2FF",
+
+      },
+      fontFamily: {
+        roboto: ["Roboto"],
+      }
+      
+    },
   },
   plugins: [],
 } satisfies Config;
