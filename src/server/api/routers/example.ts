319be64df1545a2c1fbefb4e532bb2f19f882aa1
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2023-05-06 02:45:30 -0500

    comment trpc example route for deploying test

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -15,9 +15,13 @@ export const exampleRouter = createTRPCRouter({
       };
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  getAll: publicProcedure.query(() =>
+    // { ctx }
+    {
+      return "empty";
+      // return ctx.prisma.example.findMany();
+    }
+  ),
 
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
