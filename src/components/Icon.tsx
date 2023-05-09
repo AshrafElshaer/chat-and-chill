e231744b7035776683817f7d83514b58f46c38d3
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2023-05-09 14:44:06 -0500

    sidebar search input & icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,18 +1,20 @@
+import { AiOutlineGoogle, AiFillGithub, AiOutlineSearch } from "react-icons/ai";
 
-import { AiOutlineGoogle, AiFillGithub } from "react-icons/ai";
-
-export type IconName = "google" | "github";
+export type IconName = "google" | "github" | "search";
 
 type Props = {
   iconName: IconName;
+  className?: string;
 };
 
-export default function Icon({ iconName }: Props) {
+export default function Icon({ iconName, className }: Props) {
   switch (iconName) {
     case "google":
       return <AiOutlineGoogle size="1.5rem" />;
     case "github":
       return <AiFillGithub size="1.5rem" />;
+    case "search":
+      return <AiOutlineSearch size="1.5rem" className={className} />;
     default:
       return null;
   }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import { type ChangeEvent, useState } from "react";
 import Input from "./Input";
+import Icon from "./Icon";
 
 const Sidebar = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -12,12 +13,12 @@ const Sidebar = () => {
   return (
     <aside>
       <div className="flex h-screen w-80 flex-col bg-lightBg px-4 py-8 text-primary">
-        <div className="w-full">
-
+        <div className="w-full relative ">
+        <Icon iconName="search" className="absolute top-2 left-4 text-darkGrey" />
         <Input
           placeholder="Search or start new chat"
           inputSearch={true}
-          className="w-full rounded-full bg-darkBg"
+          className="w-full rounded-full bg-darkBg pl-12"
           value={searchTerm}
           onChange={handleSearchChange}
           />
