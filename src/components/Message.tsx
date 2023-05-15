6fd7029c0bfde6b3848cf3247869bf1591c36ef7
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2023-05-15 14:36:51 -0500

    room preview to display name & last message date

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -37,7 +37,7 @@ export default MessageComponent;
 
 
 
-const getDaysAgo = (date: Date) => {
+export const getDaysAgo = (date: Date) => {
   const today = new Date();
   const diffTime = Math.abs(today.getTime() - date.getTime());
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
--- src/components/sidebar/ChatroomPreview.tsx
+++ src/components/sidebar/ChatroomPreview.tsx
@@ -4,18 +4,25 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
 
+import { getDaysAgo } from "@/components/Message";
+
 type Props = {
   room: Chatroom & { messages: Message[]; users: User[] };
 };
 
 const ChatroomPreview = ({ room }: Props) => {
   const { data: session } = useSession();
-  // get router params
   const router = useRouter();
   const { id: paramId } = router.query;
+
+  if (!room) return null;
   if (!session) return null;
   const geust = room.users.find((user) => user.id !== session.user.id);
   if (!geust) return null;
+
+  const lastMessage = room.messages.at(-1);
+  // if (!lastMessage) return null;
+  // const lastMessageDate =  new Date(lastMessage.createdAt);
   return (
     <div
       className={`flex h-20 items-center px-4 hover:bg-black ${
@@ -31,8 +38,16 @@ const ChatroomPreview = ({ room }: Props) => {
       />
       <div className="flex w-full flex-col gap-2 pl-4">
         <div className="flex justify-between">
-          <h3 className="font-semibold text-white">{room.id}</h3>
-          <span className="text-sm text-gray-400">12:00</span>
+          <h3 className="font-semibold text-white">{geust.name}</h3>
+          <span className="text-sm text-gray-400">
+          { lastMessage ?
+          getDaysAgo(new Date(lastMessage.createdAt)) > 1
+          ? `${getDaysAgo(new Date(lastMessage.createdAt))} days ago`
+          : new Date(lastMessage.createdAt).toLocaleTimeString("en-US", {
+              hour: "2-digit",
+              minute: "2-digit",
+            }) : "No messages yet"}
+          </span>
         </div>
         <span className="h-6 overflow-hidden text-ellipsis text-gray-500">
           That is so awesome !
