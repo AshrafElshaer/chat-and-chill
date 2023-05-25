a2d44ef0888259b10f54b3a3424b70878d8addbb
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2023-05-25 03:56:11 -0500

    handle friends search

--- src/components/sidebar/friends/Friends.tsx
+++ src/components/sidebar/friends/Friends.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import { type ChangeEvent, useState, useEffect } from "react";
 import { api } from "@/utils/api";
 
 import AddNewFriend from "./AddNewFriend";
 import { Button, LoadingSpinner } from "@/components";
 import UserPreview from "./UserPreview";
+import SearchBar from "../SearchBar";
+import type { User } from "@prisma/client";
 
 type Props = {
   selectedTab: "chatrooms" | "friends";
   isSidebarOpen: boolean;
 };
 const Friends = ({ selectedTab, isSidebarOpen }: Props) => {
-  const [isAddFriendOpen, setIsAddFriendOpen] = React.useState<boolean>(false);
-  const { data: friends, isLoading } = api.user.getAllFriends.useQuery();
+  const [isAddFriendOpen, setIsAddFriendOpen] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const { data: friendsResponse, isLoading } =
+    api.user.getAllFriends.useQuery();
+
+  const [friends, setFriends] = useState<User[]>(friendsResponse ?? []);
+
+  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+    setSearchTerm(value);
+
+    if (!friendsResponse) return;
+    if (value === "") return setFriends(friendsResponse);
+    const filteredFriends = friendsResponse.filter(
+      (friend) =>
+        friend.name && friend.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setFriends(filteredFriends);
+  }
+
+  useEffect(() => {
+    if (!friendsResponse) return;
+    setFriends(friendsResponse);
+  }, [friendsResponse]);
 
   if (isLoading) return <LoadingSpinner />;
 
@@ -30,6 +54,11 @@ const Friends = ({ selectedTab, isSidebarOpen }: Props) => {
           <AddNewFriend setIsAddFriendOpen={setIsAddFriendOpen} />
         ) : (
           <div className="flex h-full flex-col">
+            <SearchBar
+              placeholder="Search Your Friends"
+              handleSearchChange={handleSearch}
+              searchTerm={searchTerm}
+            />
             {friends &&
               friends.length > 0 &&
               friends.map((friend) => (
